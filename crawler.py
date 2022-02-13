ff87f00f74082e50fc385c7f50cc384c66ddd8d2
Reinhold B <reinhold.brant@gmx.de>
2022-02-13 17:47:13 +0100

    -

--- crawler.py
+++ crawler.py
@@ -15,8 +15,8 @@ def countLines(filePath):
 def count(dirPath):
     _sum = 0
     for file in os.listdir(dirPath):
-        print(file)
         path = os.path.join(dirPath, file)
+        print(path)
         if os.path.isfile(path):
             try:
                 _sum += countLines(path)
@@ -34,7 +34,6 @@ def main():
     root = Tk()
     _dir = filedialog.askdirectory(
         initialdir="/", title="Select dir")
-    print("Counting...depending on the size of the directory, you will have to wait a bit")
     _sum = count(_dir)
     print("\n\n\n" + _dir + ": " + str(_sum))
     root.destroy()
