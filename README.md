f869849fc65826ae57d0b5a4c774a937e390c16b
Reinhold B <reinhold.brant@gmx.de>
2022-02-13 17:28:06 +0100

    -

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python Directory Crawler
+
+Crawls a directory recursively and counts lines of text/code in all files in the base or subdirectory.
+Uses recursion.
+
+Open the crawler, select a directory and wait for the result.
---
+++ crawler.py
@@ -0,0 +1,43 @@
+import time
+from tkinter import Tk, filedialog
+import os
+
+
+def countLines(filePath):
+    length = 0
+    with open(filePath, 'r') as file:
+        lines = file.readlines()
+        length += len(lines)
+        file.close()
+    return length
+
+
+def count(dirPath):
+    _sum = 0
+    for file in os.listdir(dirPath):
+        path = os.path.join(dirPath, file)
+        if os.path.isfile(path):
+            try:
+                _sum += countLines(path)
+            except:
+                pass
+        elif os.path.isdir(path):
+            try:
+                _sum += count(path)
+            except:
+                pass
+    return _sum
+
+
+def main():
+    root = Tk()
+    _dir = filedialog.askdirectory(
+        initialdir="/", title="Select dir")
+    _sum = count(_dir)
+    print(_dir + ": " + str(_sum))
+    root.destroy()
+    time.sleep(1000)
+
+
+if __name__ == "__main__":
+    main()
